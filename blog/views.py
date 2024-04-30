aa0013e8be4898dcf9d28d5ea3170bfe49567232
Oriabure Numerous <noriabure@gmail.com>
2024-04-30 01:48:56 +0100

    Working on a blog site

---
+++ blog/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post':post,
+                                                    'form': form})
+
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts=paginator.page(1)
+#     except EmptyPage:
+#         posts=paginator.page(paginator.num_pages)
+
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
